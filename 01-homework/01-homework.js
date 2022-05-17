f9bddcb73852580259721bb13337212486d96e41
RadyslavVal <RValuiskyi@gmail.com>
2022-05-18 00:22:06 +0300

    fix rounded

--- 01-homework/01-homework.js
+++ 01-homework/01-homework.js
@@ -1,75 +1,75 @@
-let silver = 15.678;
-let platinum = 123.965;
-let gold = 90.2345;
-let prices = [silver, platinum, gold]
+const silver = 15.678;
+const platinum = 123.965;
+const gold = 90.2345;
+const prices = [silver, platinum, gold]
 
-let maxPrice = arr => {
+const maxPrice = arr => {
     console.log(`Максимальна ціна: ${Math.max(...arr)}`)
 }
 
-let minPrice = arr => {
+const minPrice = arr => {
     console.log(`Мінімальна ціна: ${Math.min(...arr)}`)
 }
 
-let sum = arr => {
+const sum = arr => {
     let resSum = arr.reduce((a, b) => a + b, 0)
     console.log(`Вартість всіх товарів: ${resSum}`)
     return resSum;
 }
 
-let wholeAmount = arr => {
+const wholeAmount = arr => {
     let resSum = 0
     arr.forEach((element => resSum += Math.floor(element)))
     console.log(`Сума цілої частини вартості кожного товару: ${resSum}`)
 }
 
-let roundedSum100 = arr => {
+const roundedSum100 = arr => {
     let resSum = 0
     arr.forEach((element => resSum += Math.floor(element)))
-    console.log(`Сума товарів округлена до сотень: ${Math.ceil(resSum / 100) * 100}`)
+    console.log(`Сума товарів округлена до сотень: ${Math.round(resSum / 100) * 100}`)
 }
 
-let pairOrNot = resSum => {
+const pairOrNot = resSum => {
     if (Math.floor(resSum) / 2 === 0) {
         return console.log('Чи є сума всіх товарів парною: true')
     }
     console.log('Чи є сума всіх товарів парною: false')
 }
 
-let balance = (resSum, pay) => {
+const balance = (resSum, pay) => {
     console.log(`Сума решти, якщо клієнт платить 500: ${pay - resSum}`)
 }
 
-let midlValue = arr => {
+const midlValue = arr => {
     let midlValue = arr.reduce((a, b) => a + b, 0) / arr.length;
     console.log(`Середнє значення цін: ${Number(midlValue.toFixed(2))}`)
 }
 
-let randomDiscount = (maxDiscount) => {
+const randomDiscount = (maxDiscount) => {
     let discount = Math.floor(Math.random() * maxDiscount)
     console.log(`Випадкова знижка: ${discount} %`)
     return discount;
 };
 
-let randomPay = (resSum, discount) => {
-    let sumForPayWithDisc = Number((resSum - resSum * discount / 100).toFixed(2))
-    console.log(`Сума до оплати, враховуючи випадкову знижу: ${sumForPayWithDisc}`)
-    return sumForPayWithDisc;
+const randomPay = (resSum, discount) => {
+    console.log(resSum, discount)
+    console.log(`Сума до оплати, враховуючи випадкову знижу: ${(resSum - resSum * discount).toFixed(2)}`)
+    return (resSum - resSum * discount).toFixed(2);
 }
 
-let netProfit = (resSum, pay) => {
+const netProfit = (resSum, pay) => {
     console.log(`Чистий прибуток: ${pay - resSum / 2}`)
 }
 
 maxPrice(prices);
 minPrice(prices);
-let resSum = sum(prices);
+const resSum = sum(prices);
 wholeAmount(prices);
 roundedSum100(prices);
 pairOrNot(resSum);
 balance(resSum, 500);
 midlValue(prices, 500);
-let discount = randomDiscount(100);
-let pay = randomPay(resSum, discount);
+const discount = randomDiscount(100) / 100;
+const pay = randomPay(resSum, discount);
 netProfit(resSum, pay);
 
