9375bff3cb3892d940f07372c9ef1e884d474161
RadyslavVal <RValuiskyi@gmail.com>
2022-05-19 00:28:55 +0300

    create homework02

---
+++ 02-homework/02-homework.js
@@ -0,0 +1,41 @@
+let N;
+let M;
+let parityCheck;
+
+while (!Number.isInteger(parseInt(N))) {
+    N = prompt("Enter first number");
+    console.log(N);
+};
+
+while (!Number.isInteger(parseInt(M))) {
+    M = prompt("Enter second number");
+    if (M < N) {
+        while (Number(M) < Number(N)) {
+            M = prompt("Second number must be biger then first");
+        };
+    };
+    console.log(M);
+};
+
+parityCheck = confirm("Skip pair number?") ? "TRUE" : "FALSE";
+
+let result = (N, M, parityCheck) => {
+    sum = 0;
+    for (let i = N; i <= M;) {
+        if (parityCheck === "FALSE") {
+            sum += i;
+            i++;
+        }
+        else if (i % 2 != 0) {
+            sum += i;
+            i += 2;
+        }
+        else {
+            i++;
+        }
+    }
+    return console.log(sum)
+}
+
+result(parseInt(N), parseInt(M), parityCheck)
+
