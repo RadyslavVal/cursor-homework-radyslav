9c3b3ec21a8314226afdbcdb7af567c9ca71c336
RadyslavVal <RValuiskyi@gmail.com>
2022-05-25 16:07:15 +0300

    add hw3 folder

---
+++ 03-homework/main.js
@@ -0,0 +1,43 @@
+const getMaxDigit = number => {
+    return
+}
+
+const degreeOf = number => {
+    return
+}
+
+const nameFormater = number => {
+    return
+}
+
+const netSalary = number => {
+    return
+}
+
+const randomNumberFromRange = number => {
+    return
+}
+
+const numberOfRepetitions = number => {
+    return
+}
+
+const converterUsdUah = number => {
+    return
+}
+
+const generatorPasswords = number => {
+    return
+}
+
+const deleteLetters = number => {
+    return
+}
+
+const isPalyndrom = number => {
+    return
+}
+
+const deleteDuplicateLetters = number => {
+    return
+}
