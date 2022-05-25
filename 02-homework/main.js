2c538c609bc13af40949ace74fc56d850afb4765
RadyslavVal <RValuiskyi@gmail.com>
2022-05-25 18:55:16 +0300

    update hw2 +prompt

--- 02-homework/main.js
+++ 02-homework/main.js
@@ -3,11 +3,11 @@ let M;
 let parityCheck;
 
 while (!Number.isInteger(parseInt(N))) {
-    N = prompt("Enter first number");
+    N = +prompt("Enter first number");
 };
 
-while (!Number.isInteger(parseInt(M)) || +M < +N) {
-    M = prompt("Enter second number(must be biger then first)");
+while (!Number.isInteger(parseInt(M)) || M < N) {
+    M = +prompt("Enter second number(must be biger then first)");
 };
 
 parityCheck = confirm("Skip pair number?");
