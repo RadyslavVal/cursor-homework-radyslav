a5730c702024f1c29ec224d1bd1e3f74e3c210f3
RadyslavVal <RValuiskyi@gmail.com>
2022-05-26 19:12:13 +0300

    hw4 done

--- 04-homework/main.js
+++ 04-homework/main.js
@@ -1,43 +1,74 @@
-const getMaxDigit = number => {
-    return
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const marks = [4, 5, 5, 3, 4, 5];
+
+const getRandomNumberFromRange = (N, M) => {
+    if (isNaN(N) || isNaN(M)) {
+        return "Enter only numbers"
+    }
+
+    let min = Math.ceil(Math.min(N, M));
+    let max = Math.floor(Math.max(N, M));
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const degreeOf = number => {
-    return
-}
+const getPairs = arrStudents => {
+    let girls = [];
+    let boys = [];
+    let resPairs = [];
 
-const nameFormater = number => {
-    return
-}
+    for (let i = 0; i < arrStudents.length; i++) {
+        let nameLength = arrStudents[i].length;
+        if (arrStudents[i].charAt(nameLength - 1) == 'а') {
+            girls.push(arrStudents[i])
+        }
+        else boys.push(arrStudents[i])
+    };
 
-const netSalary = number => {
-    return
-}
+    for (let i = 0; i < boys.length; i++) {
+        resPairs.push([boys[i], girls[i]])
+    };
 
-const randomNumberFromRange = number => {
-    return
-}
+    return resPairs;
+};
 
-const numberOfRepetitions = number => {
-    return
-}
+const getPairsThemes = (arrPairs, arrThemes) => {
+    let pairsThemes = [];
 
-const converterUsdUah = number => {
-    return
-}
+    for (let i = 0; i < arrThemes.length; i++) {
+        pairsThemes.push([`${arrPairs[i][0]} і ${arrPairs[i][1]}`, arrThemes[i]])
+    };
 
-const generatorPasswords = number => {
-    return
-}
+    return pairsThemes;
+};
 
-const deleteLetters = number => {
-    return
-}
+const getMarks = (arrStudents, arrMarks) => {
+    let studentsMarks = [];
 
-const isPalyndrom = number => {
-    return
-}
+    for (let i = 0; i < arrStudents.length; i++) {
+        studentsMarks.push([arrStudents[i], arrMarks[i]])
+    };
 
-const deleteDuplicateLetters = number => {
-    return
-}
+    return studentsMarks;
+};
+
+const getPairsMarks = (arrPairsThemes) => {
+    let pairsMarks = [];
+
+    for (let i = 0; i < arrPairsThemes.length; i++) {
+        pairsMarks.push([...arrPairsThemes[i], getRandomNumberFromRange(1, 5)])
+    };
+
+    return pairsMarks;
+};
+
+const pairs = getPairs(students);
+console.log(pairs);
+const pairsThemes = getPairsThemes(pairs, themes);
+console.log(pairsThemes);
+const studentMarks = getMarks(students, marks);
+console.log(studentMarks);
+const pairsMarks = getPairsMarks(pairsThemes);
+console.log(pairsMarks);
+
