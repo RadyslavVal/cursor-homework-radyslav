b9ae2ea6312c352cfcc4e24fa0a12f53e357f3e2
RadyslavVal <RValuiskyi@gmail.com>
2022-06-21 18:20:06 +0300

    add buttons

--- 11-homework/main.js
+++ 11-homework/main.js
@@ -2,16 +2,34 @@ const getRandomValues = (min, max) => {
     return Math.floor(Math.random() * (Math.floor(Math.max(min, max)) - Math.ceil(Math.min(min, max)) + 1)) + min;
 };
 
+
 let app = document.querySelector('.app');
 let square = document.createElement('div');
 square.id = 'square';
+let buttonBlock = document.createElement('div');
+buttonBlock.className = "d-flex";
+app.appendChild(buttonBlock)
+
+let buttonBlocks = document.createElement('button');
+buttonBlocks.className = "btn btn-success";
+buttonBlocks.textContent = 'generateBlocks()';
+buttonBlocks.onclick = generateBlocks;
+
+let buttonBlocksInterval = document.createElement('button');
+buttonBlocksInterval.className = "btn btn-warning";
+buttonBlocksInterval.textContent = 'generateBlocksInterval()';
+buttonBlocksInterval.onclick = generateBlocksInterval;
+
+buttonBlock.appendChild(buttonBlocks);
+buttonBlock.appendChild(buttonBlocksInterval);
+
 
 let nIntervId;
-
 function stopColor() {
     clearInterval(nIntervId);
     nIntervId = null;
-}
+};
+
 
 function blocks() {
     app.appendChild(square);
@@ -19,7 +37,7 @@ function blocks() {
 
     if (squareBlock.hasChildNodes) {
         squareBlock.replaceChildren();
-    }
+    };
 
     for (i = 0; i < 25; i++) {
         let squares = document.createElement('div');
@@ -27,7 +45,8 @@ function blocks() {
         squares.style.backgroundColor = `rgb(${getRandomValues(0, 255)}, ${getRandomValues(0, 255)}, ${getRandomValues(0, 255)})`;
         squareBlock.append(squares);
     };
-}
+};
+
 
 function generateBlocks() {
     stopColor();
@@ -38,5 +57,6 @@ function generateBlocks() {
 function generateBlocksInterval() {
     if (!nIntervId) {
         nIntervId = setInterval(blocks, 1000);
-    }
-}; 
+    };
+};
+
