d04eab9b442ac194c47f0c0de5ba33c9e883ae19
RadyslavVal <RValuiskyi@gmail.com>
2022-07-17 21:44:29 +0300

    hw18 ver.1

---
+++ 18-homework/src/components/Post/Post.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Post.css'
+
+function Post({ data }) {
+    const { author, content, image, date } = data;
+    const { name, photo, nickname } = author;
+    return (
+        <div className="post">
+            <div className="post-card">
+                <div className="post-header">
+                    <div className="title-avatar">
+                        <img src={author.avatar} alt="Avatar" />
+                    </div>
+                    <div className='post-content'>
+                        <div className="post-title">
+                            <div className="author-info">
+                                <div className="author-nameSername">{author.name}</div>
+                                <div className="author-nickname">{author.nickname}</div>
+                            </div>
+                            <div className="post-date">{data.date}</div>
+                        </div>
+                        <div className="post-text">{data.content}</div>
+                    </div>
+                </div>
+                <div className="post-image">
+                    <img src={data.image} alt="Content Image" />
+                </div>
+                <div className="post-panel"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
